3cc56fc92321ba22fd4db41ac3c09268e19c902c
daonguyendev <37641075+daonguyendev@users.noreply.github.com>
2023-05-13 06:49:14 +0700

    Update BulletController.js

--- controller/BulletController.js
+++ controller/BulletController.js
@@ -16,7 +16,7 @@ export default class BulletController {
 
   draw(ctx) {
     this.bullets = this.bullets.filter(
-      (bullet) => bullet.y + bullet.width > 0 && bullet.y <= this.canvas.height
+      (bullet) => bullet.y + bullet.height > 0 && bullet.y <= this.canvas.height
     );
 
     this.bullets.forEach((bullet) => bullet.draw(ctx));
